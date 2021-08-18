93f4e0b93e981bebcd605f20bd83d3fee85df19e
haejii <yanghagi@naver.com>
2021-08-18 22:42:40 +0900

    First Commit

---
+++ app/src/main/java/com/basic/diceapplication/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.basic.diceapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
