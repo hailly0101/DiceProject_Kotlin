d6423d0a21a6dca939c81ff46812462625047fb4
haejii <yanghagi@naver.com>
2021-08-21 22:21:05 +0900

    Implement Data Binding

--- app/src/main/java/com/basic/diceapplication/MainActivity.kt
+++ app/src/main/java/com/basic/diceapplication/MainActivity.kt
@@ -2,11 +2,32 @@ package com.basic.diceapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.basic.diceapplication.databinding.ActivityMainBinding
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        binding.startBtn.setOnClickListener{
+
+            Toast.makeText(this, "주사위 Go!!", Toast.LENGTH_LONG).show()
+
+
+//            Log.d("MainActivity", Random.nextInt(1, 6).toString())
+//            Random.nextInt(1, 6)
+//            Random.nextInt(1, 6)
+
+
+        }
     }
 }
